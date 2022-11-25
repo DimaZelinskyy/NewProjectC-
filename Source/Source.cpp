10432545911f0f3ae1273172083dac10ab27eb61
Dima <dimazelinskiy1@gmail.com>
2022-11-25 19:24:31 +0200

    Second commit

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -1,20 +1,34 @@
-// Source.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+/*Створити новий С++ проєкт, описати карказ роєкту(бібліотеки, головну функцію) 
+    -Ствоприти віддалений репозиторій 
+    - Запушити проєкт на віддалений сервер 
+    По функціональності: 
+    -Клас фігура(Вектор з точок) 
+    - Створити декілька фігур, вивести на екран у мейні. 
+    - Створити хоча 5 комітів 
+    Пізніше до функціоналу додати: 
+    -Клас фігура - абстрактний чисто віртуальний метод прінт 
+    -4 похідні класи фігур 
+    - в мейні створити масив з різних фігур і вивести інформацію про них.*/
+
+
+class figure {
+    vector<string> points;
+public:
+    figure() : points('*') {}
+
+};
+
+
+int main() {
+
+
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
