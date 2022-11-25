29af5b8457a6fe0910efb1d64f6d3ea37423d66f
Dima <dimazelinskiy1@gmail.com>
2022-11-25 19:45:42 +0200

    Third commit

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -19,15 +19,70 @@ using namespace std;
 
 
 class figure {
+protected:
     vector<string> points;
 public:
-    figure() : points('*') {}
-
+    figure() { points.resize(10); 
+    for (int i = 0; i < 10; i++)
+    {
+        points[i] = '*';
+    }
+    }
+    virtual void print() = 0;
+};
+class rectangle : public figure{
+public:
+    rectangle() {}
+    void print() override {
+        cout << "********" << endl;
+        cout << "*      *" << endl;
+        cout << "*      *" << endl;
+        cout << "********" << endl;
+    }
+};
+class square : public figure {
+public:
+    square() {}
+    void  print() override {
+        cout << "****" << endl;
+        cout << "*  *" << endl;
+        cout << "****" << endl;
+    }
+};
+class triangle : public figure {
+public:
+    triangle() {}
+    void print() override {
+        cout << "   * " << endl;
+        cout << " *   *" << endl;
+        cout << "*******" << endl;
+    }
+};
+class circle : public figure {
+public:
+    circle() {}
+    void print()override {
+        cout << "   *   " << endl;
+        cout << " *   * " << endl;
+        cout << "   *   " << endl;
+    }
 };
 
 
 int main() {
-
+    figure* fptr;
+    rectangle r;
+    square s;
+    triangle t;
+    circle c;
+    fptr = &r;
+    fptr->print();
+    fptr = &s;
+    fptr->print();
+    fptr = &t;
+    fptr->print();
+    fptr = &c;
+    fptr->print();
 
 
 	return 0;
